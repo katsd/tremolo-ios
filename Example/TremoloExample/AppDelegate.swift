659ad359ec63b3d150feb46edcd108d42a13a9c6
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-22 20:07:55 +0900

    Rename

--- Example/TremoloExample/AppDelegate.swift
+++ Example/TremoloExample/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  TremoloTest
+//  TremoloExample
 //
 //  Created by Katsu Matsuda on 2020/03/22.
 //  Copyright © 2020 Katsu Matsuda. All rights reserved.
--- Example/TremoloExample/ContentView.swift
+++ Example/TremoloExample/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  TremoloTest
+//  TremoloExample
 //
 //  Created by Katsu Matsuda on 2020/03/22.
 //  Copyright © 2020 Katsu Matsuda. All rights reserved.
