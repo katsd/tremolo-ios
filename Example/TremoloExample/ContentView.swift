26002f13c2da3e300b3ee1cf67a51ddd2aecdf6c
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-23 21:24:28 +0900

    Update Example

--- Example/TremoloExample/ContentView.swift
+++ Example/TremoloExample/ContentView.swift
@@ -9,9 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, World!")
+        TremoloView(Tremolo())
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
