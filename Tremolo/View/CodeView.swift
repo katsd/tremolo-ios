1a059ba71fffd085576b31948a4f8e697858a391
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-29 17:19:13 +0900

    Add CodeView

---
+++ Tremolo/View/CodeView.swift
@@ -0,0 +1,21 @@
+//
+//  CodeView.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/03/29.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct CodeView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> UIView {
+        fatalError("makeUIView(context:) has not been implemented")
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {
+
+    }
+
+}
