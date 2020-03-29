45d1ffdca6c309b7d26a1376424c92444946f523
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-29 20:30:48 +0900

    Add ArgView

---
+++ Tremolo/View/ArgView.swift
@@ -0,0 +1,17 @@
+//
+//  ArgView.swift
+//  Example
+//  
+//  Created by Katsu Matsuda on 2020/03/29.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ArgView: UIView {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
