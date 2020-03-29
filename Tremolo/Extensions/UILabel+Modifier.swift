ad90e9d20e4263c9cb47878a9bf4e624c93c95b1
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-29 20:42:44 +0900

    Add Modifiers

---
+++ Tremolo/Extensions/UILabel+Modifier.swift
@@ -0,0 +1,18 @@
+//
+//  UILabel+Modifier.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/03/29.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+
+    func text(_ text: String) -> UILabel {
+        self.text = text
+        return self
+    }
+
+}
---
+++ Tremolo/Extensions/UIStackView+Modifier.swift
@@ -0,0 +1,33 @@
+//
+//  UIStackView+Modifier.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/03/29.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+extension UIStackView {
+
+    func axis(_ axis: NSLayoutConstraint.Axis) -> UIStackView {
+        self.axis = axis
+        return self
+    }
+
+    func distribution(_ distribution: UIStackView.Distribution) -> UIStackView {
+        self.distribution = distribution
+        return self
+    }
+
+    func alignment(_ alignment: UIStackView.Alignment) -> UIStackView {
+        self.alignment = alignment
+        return self
+    }
+
+    func spacing(_ spacing: CGFloat) -> UIStackView {
+        self.spacing = spacing
+        return self
+    }
+
+}
