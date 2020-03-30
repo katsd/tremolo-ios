1d277a285d82235bd3ad43ae103811dbf68a9a06
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-30 18:56:51 +0900

    Update UIScrollView+Modifier

--- Tremolo/Extensions/Modifiers/UIScrollView+Modifier.swift
+++ Tremolo/Extensions/Modifiers/UIScrollView+Modifier.swift
@@ -10,4 +10,39 @@ import UIKit
 
 extension UIScrollView {
 
+    func isScrollEnabled(_ flag: Bool) -> UIScrollView {
+        self.isScrollEnabled = flag
+        return self
+    }
+
+    func isDirectionalLockEnabled(_ flag: Bool) -> UIScrollView {
+        self.isDirectionalLockEnabled = flag
+        return self
+    }
+
+    func isPagingEnabled(_ flag: Bool) -> UIScrollView {
+        self.isPagingEnabled = flag
+        return self
+    }
+
+    func scrollsToTop(_ flag: Bool) -> UIScrollView {
+        self.scrollsToTop = flag
+        return self
+    }
+
+    func bounces(_ flag: Bool) -> UIScrollView {
+        self.bounces = flag
+        return self
+    }
+
+    func alwaysBounceVertical(_ flag: Bool) -> UIScrollView {
+        self.alwaysBounceVertical = flag
+        return self
+    }
+
+    func alwaysBounceHorizontal(_ flag: Bool) -> UIScrollView {
+        self.alwaysBounceHorizontal = flag
+        return self
+    }
+
 }
