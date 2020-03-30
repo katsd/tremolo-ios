78e45d7bae8ee8ffdb31cda8f893d3aba64b1dd8
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-30 18:50:25 +0900

    UIScrollView+Modifier

---
+++ Tremolo/Extensions/Modifiers/UIScrollView+Modifier.swift
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+Modifier.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/03/30.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+extension UIScrollView {
+
+}
