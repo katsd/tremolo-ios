ca0d8f4afd9e111cb36590b4033b7536956529f7
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-31 20:51:06 +0900

    Add SelectedBlockPos

---
+++ Tremolo/View/SelectedBlockPos.swift
@@ -0,0 +1,17 @@
+//
+//  SelectedBlockPos.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/03/31.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+struct SelectedBlockPos {
+
+    let blockView: BlockView
+
+    let idx: Int
+
+}
