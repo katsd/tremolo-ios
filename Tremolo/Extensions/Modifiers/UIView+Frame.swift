eceeae593ff74a992307ed2303f7e55450212c23
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-31 21:43:00 +0900

    Add UIView+Frame

---
+++ Tremolo/Extensions/Modifiers/UIView+Frame.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Frame.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/03/31.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    var globalFrame: CGRect {
+        convert(frame, from: nil)
+    }
+
+}
