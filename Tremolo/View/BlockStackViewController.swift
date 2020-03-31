d47bbce50916b113ae2ca89a1f27a4adc052c37b
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-31 21:23:35 +0900

    Add BlockStackViewController

---
+++ Tremolo/View/BlockStackViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BlockStackViewController.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/03/31.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+protocol BlockStackViewController {
+
+}
