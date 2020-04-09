9aa32834625194ab783e895c5d803487b87fdd19
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-09 23:45:37 +0900

    Add Blur

---
+++ Tremolo/View/UIViewRepresentable/Blur.swift
@@ -0,0 +1,22 @@
+//
+//  Blur.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/04/09.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct Blur: UIViewRepresentable {
+
+    var style: UIBlurEffect.Style = .systemMaterial
+
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
