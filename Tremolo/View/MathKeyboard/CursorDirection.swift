527e35d1ed650fb975c097509247beda4183f2ef
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-09 21:14:15 +0900

    Add CursorDirection

---
+++ Tremolo/View/MathKeyboard/CursorDirection.swift
@@ -0,0 +1,17 @@
+//
+//  CursorDirection.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/04/09.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import Foundation
+
+enum CursorDirection {
+
+    case back
+
+    case forward
+
+}
