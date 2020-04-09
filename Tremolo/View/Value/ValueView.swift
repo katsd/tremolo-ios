51d1ce9d9a02f16f22ad91c94f2964c8e5e2c328
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-09 17:45:52 +0900

    Add ValueView

---
+++ Tremolo/View/Value/ValueView.swift
@@ -0,0 +1,13 @@
+//
+//  ValueView.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/04/09.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ValueView: UIView {
+
+}
