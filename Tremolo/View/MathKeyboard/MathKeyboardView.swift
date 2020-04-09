c135a7c8bedbf470ee2d57b4307c563bd15b59ee
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-09 20:58:48 +0900

    Add MathKeyboardView

---
+++ Tremolo/View/MathKeyboard/MathKeyboardView.swift
@@ -0,0 +1,17 @@
+//
+//  MathKeyboardView.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/04/09.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct MathKeyboardView: View {
+
+    var body: some View {
+        Spacer()
+    }
+
+}
