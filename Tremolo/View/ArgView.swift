375e242e12488bb148fb2d3bf6e029968f0b7327
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-10 21:37:22 +0900

    Update ValueView

--- Tremolo/View/ArgView.swift
+++ Tremolo/View/ArgView.swift
@@ -12,6 +12,17 @@ class ArgView: UIView {
 
     init(arg: Argument) {
         super.init(frame: .zero)
+
+        switch arg {
+        case let .value(v):
+            let valueView = ValueView(value: v)
+            addSubview(valueView)
+            valueView.equalTo(self)
+
+        default:
+            break
+        }
+
     }
 
     required init?(coder: NSCoder) {
--- Tremolo/View/Value/ValueView.swift
+++ Tremolo/View/Value/ValueView.swift
@@ -10,4 +10,63 @@ import UIKit
 
 class ValueView: UIView {
 
+    let stackView = UIStackView(frame: .zero)
+
+    init(value: Value) {
+        super.init(frame: .zero)
+
+        "1+2*3".forEach {
+            let label = UILabel()
+                .text(String($0))
+                .textColor(.black)
+
+            self.stackView.addArrangedSubview(label)
+        }
+
+        addSubview(stackView)
+        stackView.equalTo(self, inset: .init(top: 3, left: 3, bottom: 3, right: 3))
+
+        setStyle()
+
+        setGesture()
+
+        MathKeyboard.receiver = self
+    }
+
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+
+    private func setStyle() {
+        self.backgroundColor(.white)
+            .cornerRadius(5)
+    }
+
+    private func setGesture() {
+        self.isUserInteractionEnabled = true
+
+        self.tap { _ in
+            MathKeyboard.openKeyboard()
+        }
+    }
+}
+
+extension ValueView: MathKeyboardReceiver {
+
+    func addTexts(_ texts: [String], cursor: Int) {
+        print(texts)
+    }
+
+    func delete() {
+        print("Delete")
+    }
+
+    func endEditing() {
+        print("Return")
+    }
+
+    func moveCursor(_ direction: CursorDirection) {
+        print("Move")
+    }
+
 }
