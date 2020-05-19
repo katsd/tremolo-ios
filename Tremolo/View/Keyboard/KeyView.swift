d6d77de77afcffd0fe4645483791231c03a638f3
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-19 19:17:27 +0900

    Add KeyView

---
+++ Tremolo/View/Keyboard/KeyView.swift
@@ -0,0 +1,40 @@
+//
+//  KeyView.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/05/19.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct KeyView<Content: View>: View {
+
+    let value: KeyValue
+
+    let size: CGSize
+
+    let label: () -> Content
+
+    init(value: KeyValue, size: CGSize, @ViewBuilder label: @escaping () -> Content) {
+        self.value = value
+        self.size = size
+        self.label = label
+    }
+
+    var body: some View {
+        Button(action: {
+            switch self.value {
+            case let .text(texts: texts, cursor: cursor):
+                Keyboard.mathKeyboardReceiver?.addTexts(texts, cursor: cursor)
+            case let .action(action):
+                action()
+            }
+        }) {
+            label()
+        }
+            .frame(width: size.width, height: size.height)
+            .cornerRadius(7)
+    }
+
+}
