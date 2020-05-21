431c1c6673314593123c00ce11f9d5798c6813f9
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-21 22:53:40 +0900

    Add UIButton.contentEdgeInsets()

---
+++ Tremolo/Extensions/Modifiers/UIButton+Modifier.swift
@@ -0,0 +1,19 @@
+//
+//  UIButton+Modifier.swift
+//  Tremolo
+//  
+//  Created by Katsu Matsuda on 2020/05/21.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+
+    func contentEdgeInsets(_ insets: UIEdgeInsets) -> UIButton {
+        contentEdgeInsets = insets
+        return self
+    }
+
+
+}
