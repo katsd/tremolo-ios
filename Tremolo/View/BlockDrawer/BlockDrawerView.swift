ff37b626eb8f3ec36af6c562766aeaef5784043d
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-24 22:27:26 +0900

    Update BlockDrawerView

--- Tremolo/View/BlockDrawer/BlockDrawerView.swift
+++ Tremolo/View/BlockDrawer/BlockDrawerView.swift
@@ -19,55 +19,56 @@ struct BlockDrawerView: View {
     let blockController: BlockController
 
     var body: some View {
-        ZStack {
-            HStack(spacing: 0) {
-                Color.clear
-                VStack {
-                    BlockSelectViewRepresentable(tremolo: tremolo, blockController: blockController)
-                }
-                    .frame(width: drawerWidth)
-                    .background(Blur(style: .systemMaterial))
-                    .transition(.move(edge: .trailing))
-            }
-
-            VStack {
-                Spacer()
-                HStack {
-                    Button(action: {
-                        withAnimation {
-                            self.showDrawer.toggle()
-                            if self.drawerWidth > 0 {
-                                self.drawerWidth = 0
-                            } else {
-                                self.drawerWidth = 250
-                            }
-                        }
-                    }) {
-                        VStack {
-                            if showDrawer {
-                                Image(systemName: "xmark")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.white)
-                                    .transition(.opacity)
-                            } else {
-                                Image(systemName: "plus")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.white)
-                                    .transition(.opacity)
-                            }
-                        }
-                            .frame(width: 20, height: 20)
-                            .padding(15)
+        GeometryReader { geo in
+            ZStack {
+                HStack(spacing: 0) {
+                    Color.clear
+                    VStack {
+                        BlockSelectViewRepresentable(tremolo: self.tremolo, blockController: self.blockController)
                     }
-                        .background(Color.blue)
-                        .cornerRadius(15)
-                        .padding(20)
+                        .frame(width: self.drawerWidth)
+                        .background(Blur(style: .systemMaterial))
+                        .transition(.move(edge: .trailing))
+                }
+
+                VStack {
                     Spacer()
+                    HStack {
+                        Button(action: {
+                            withAnimation {
+                                self.showDrawer.toggle()
+                                if self.drawerWidth > 0 {
+                                    self.drawerWidth = 0
+                                } else {
+                                    self.drawerWidth = geo.size.width * 0.6
+                                }
+                            }
+                        }) {
+                            VStack {
+                                if self.showDrawer {
+                                    Image(systemName: "xmark")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .foregroundColor(.white)
+                                        .transition(.opacity)
+                                } else {
+                                    Image(systemName: "plus")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .foregroundColor(.white)
+                                        .transition(.opacity)
+                                }
+                            }
+                                .frame(width: 20, height: 20)
+                                .padding(15)
+                        }
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                            .padding(20)
+                        Spacer()
+                    }
                 }
             }
         }
     }
-
 }
