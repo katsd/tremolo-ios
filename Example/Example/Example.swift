c86abaa9db6541050c856f4c78b2fcffbf8538bf
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-28 21:03:04 +0900

    Update Example

--- Example/Example/Example.swift
+++ Example/Example/Example.swift
@@ -15,7 +15,7 @@ class Example {
             Block(name: "assign",
                   type: .void,
                   argValues: [.variable(Variable(type: .custom("type"), name: "var")),
-                              .value(Value(type: .custom("type"), value: "128"))],
+                              .value(Value(type: .custom("type"), value: [.raw("128"), .variable(.init(type: .custom(""), name: "testVar")), .raw("Yay")]))],
                   contents: [[.label("Set"), .arg(0), .label("to"), .arg(1)]],
                   declarableVariableIndex: 0),
 
@@ -26,74 +26,36 @@ class Example {
 
             Block(name: "nyan",
                   type: .void,
-                  argValues: [.variable(Variable(type: .custom("type"), name: "var")), .value(Value(type: .custom("type"), value: "0"))],
+                  argValues: [.variable(Variable(type: .custom("type"), name: "var")), .value(Value(type: .custom("type"), value: [.raw("0")]))],
                   contents: [[.label("Test"), .arg(0)], [.label("2"), .arg(1)]]),
-
-            Block(name: "piyo",
-                  type: .void,
-                  argValues: [.variable(Variable(type: .custom("type"), name: "var"))],
-                  contents: [[.label("Test"), .arg(0), .label("3")]]),
 
             Block(name: "nyan",
                   type: .void,
                   argValues: [.code([
                                         Block(name: "yay",
                                               type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
+                                              argValues: [.value(Value(type: .custom("type"), value: []))],
                                               contents: [[.label("Content"), .arg(0), .label("0")]]),
                                         Block(name: "yay",
                                               type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
+                                              argValues: [.value(Value(type: .custom("type"), value: []))],
                                               contents: [[.label("Content"), .arg(0), .label("1")]])
                                     ])],
                   contents: [[.label("Bracket 0")], [.arg(0)]]),
 
-
             Block(name: "nyan",
                   type: .void,
                   argValues: [.code([
                                         Block(name: "yay",
                                               type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
+                                              argValues: [.value(Value(type: .custom("type"), value: []))],
                                               contents: [[.label("Content"), .arg(0), .label("0")]]),
                                         Block(name: "yay",
                                               type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
+                                              argValues: [.value(Value(type: .custom("type"), value: []))],
                                               contents: [[.label("Content"), .arg(0), .label("1")]])
                                     ])],
-                  contents: [[.label("Bracket 1")], [.arg(0)]]),
-
-
-            Block(name: "nyan",
-                  type: .void,
-                  argValues: [.code([
-                                        Block(name: "yay",
-                                              type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
-                                              contents: [[.label("Content"), .arg(0), .label("0")]]),
-                                        Block(name: "yay",
-                                              type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
-                                              contents: [[.label("Content"), .arg(0), .label("1")]])
-                                    ])],
-                  contents: [[.label("Bracket 1")], [.arg(0)]]),
-
-
-            Block(name: "nyan",
-                  type: .void,
-                  argValues: [.code([
-                                        Block(name: "yay",
-                                              type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
-                                              contents: [[.label("Content"), .arg(0), .label("0")]]),
-                                        Block(name: "yay",
-                                              type: .void,
-                                              argValues: [.value(Value(type: .custom("type"), value: ""))],
-                                              contents: [[.label("Content"), .arg(0), .label("1")]])
-                                    ])],
-                  contents: [[.label("Bracket 1")], [.arg(0)]]),
-
-
+                  contents: [[.label("Bracket 0")], [.arg(0)]]),
         ],
         variables: [
             Variable(type: .custom("value"), name: "gvar1"),
