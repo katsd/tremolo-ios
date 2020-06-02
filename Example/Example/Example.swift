22109c29cb89d7db506d2ec1136ae28cb3d32f3d
Katsu Matsuda <kats.mtd@gmail.com>
2020-06-02 23:46:34 +0900

    Update Example

--- Example/Example/Example.swift
+++ Example/Example/Example.swift
@@ -10,58 +10,69 @@ import Foundation
 
 class Example {
 
+    /*
+    static let testBlock = Block(name: "test",
+                                 type: .void,
+                                 argValues: [.variable(.init(type: .custom(""), name: "var")), .mathValue(.init(value: []))],
+                                 contents: [[.label("🧪test"), .arg(0), .arg(1)]])
+                                 */
+
     static let tremolo = Tremolo(
         blocks: [
             Block(name: "assign",
                   type: .void,
                   argValues: [.variable(Variable(type: .custom("type"), name: "var")),
                               .value(.init(type: .custom(""),
-                                           value: [.string("128"),
-                                                   .variable(type: .custom(""), name: "testVar"),
-                                                   .init(name: "inArg", type: .custom(""), argValues: [.value(.init(type: .custom(""), value: [.string("1024")]))], contents: [[.label("Func"), .arg(0)]]),
-                                                   .string("Yay")]))],
+                                           blocks: [.string("128"),
+                                                    .variable(type: .custom(""), name: "testVar"),
+                                                    .init(name: "inArg", type: .custom(""), argValues: [.value(.init(type: .custom(""), blocks: [.string("1024")]))], contents: [[.label("Func"), .arg(0)]]),
+                                                    .string("Yay")]))],
                   contents: [[.label("Set"), .arg(0), .label("to"), .arg(1)]],
                   declarableVariableIndex: 0),
 
             Block(name: "test",
                   type: .void,
-                  argValues: [.variable(Variable(type: .custom("type"), name: "var"))],
-                  contents: [[.label("Test"), .arg(0), .label("1")]]),
+                  argValues: [
+                      .mathValue(
+                          MathValueGenerator.generate {
+                              MathValueGenerator.string("1 + 2")
+                              MathValueGenerator.variable(.init(type: .custom(""), name: "var"))
+                          }
+                      )
 
-            Block(name: "nyan",
-                  type: .void,
-                  argValues: [.variable(Variable(type: .custom("type"), name: "var")), .mathValue(.init(value: [.raw("0")]))],
-                  contents: [[.label("Test"), .arg(0)], [.label("2"), .arg(1)]]),
+                  ],
+                  contents: [[.label("Test"), .arg(0)]]),
 
-            Block(name: "nyan",
-                  type: .void,
-                  argValues: [.code(
-                      .init([
-                                Block(name: "yay",
-                                      type: .void,
-                                      argValues: [.mathValue(.init(value: []))],
-                                      contents: [[.label("Content"), .arg(0), .label("0")]]),
-                                Block(name: "yay",
-                                      type: .void,
-                                      argValues: [.mathValue(.init(value: []))],
-                                      contents: [[.label("Content"), .arg(0), .label("0")]]),
-                            ]))],
-                  contents: [[.label("Bracket 0")], [.arg(0)]]),
-
-            Block(name: "nyan",
+            Block(name: "blacket",
                   type: .void,
                   argValues: [.code(
                       .init([
-                                Block(name: "yay",
+                                Block(name: "test",
                                       type: .void,
                                       argValues: [.mathValue(.init(value: []))],
-                                      contents: [[.label("Content"), .arg(0), .label("0")]]),
-                                Block(name: "yay",
+                                      contents: [[.label("test"), .arg(0)]]),
+                                Block(name: "test",
                                       type: .void,
                                       argValues: [.mathValue(.init(value: []))],
-                                      contents: [[.label("Content"), .arg(0), .label("0")]]),
+                                      contents: [[.label("test"), .arg(0)]]),
                             ]))],
-                  contents: [[.label("Bracket 0")], [.arg(0)]]),
+                  contents: [[.label("Bracket")], [.arg(0)]]),
+
+            Block(name: "blacket",
+                  type: .void,
+                  argValues: [.code(
+                      .init([
+                                Block(name: "test",
+                                      type: .void,
+                                      argValues: [.mathValue(.init(value: []))],
+                                      contents: [[.label("test"), .arg(0)]]),
+                                Block(name: "test",
+                                      type: .void,
+                                      argValues: [.mathValue(.init(value: []))],
+                                      contents: [[.label("test"), .arg(0)]]),
+                            ]))],
+                  contents: [[.label("Bracket")], [.arg(0)]]),
+
         ],
         variables: [
             Variable(type: .custom("value"), name: "gvar1"),
