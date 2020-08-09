9eff699026488417a865abc7d817e1459d774c1f
Katsu Matsuda <kats.mtd@gmail.com>
2020-08-09 23:37:40 +0900

    Add BlockStyle.defaultStyle

--- Tremolo/BlockStyle.swift
+++ Tremolo/BlockStyle.swift
@@ -22,4 +22,11 @@ struct BlockStyle {
         self.shadow = shadow
     }
 
+    static var defaultStyle: BlockStyle {
+        BlockStyle(color:
+                   UIColor.dynamicColor(light: UIColor(red: 1, green: 1, blue: 1, alpha: 1),
+                                        dark: UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1))
+        )
+    }
+
 }
